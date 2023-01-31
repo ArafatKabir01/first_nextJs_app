078132c7527fb76e4575975cc181f2f85ebfadd9
Arshiat7 <nabil786.ak@gmail.com>
2023-01-31 19:21:21 +0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,23 @@
 import Head from 'next/head'
 import { useEffect, useRef, useState } from 'react';
 import "node_modules/video-react/dist/video-react.css";
-import { Player } from 'video-react';
-
+import ReactPlayer from 'react-player'
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [emailPosition, setEmailPosition] = useState({ x: 0, y: 0, directionX: 1, directionY: 1 });
   const videoRef = useRef(null);
-console.log(emailPosition)
+  console.log(emailPosition)
   useEffect(() => {
     let timeoutId;
     if (isPlaying) {
       const updateEmailPosition = () => {
-        const videoRect = videoRef.current.getBoundingClientRect();
+        const videoRect = videoRef?.current?.getBoundingClientRect();
         console.log(videoRect)
         const x = emailPosition.x + emailPosition.directionX;
         const y = emailPosition.y + emailPosition.directionY;
         if (x + videoRect.width >= videoRect.right || x <= videoRect.left) {
           setEmailPosition({
-            x:20,
+            x: 20,
             y,
             directionX: -emailPosition.directionX,
             directionY: emailPosition.directionY,
@@ -27,20 +26,20 @@ console.log(emailPosition)
         if (y + videoRect.height >= videoRect.bottom || y <= videoRect.top) {
           setEmailPosition({
             x,
-            y:20,
+            y: 20,
             directionX: emailPosition.directionX,
             directionY: -emailPosition.directionY,
           });
-        }  if (emailPosition.x + 80 > videoRect.width || emailPosition.x + 80 > videoRect.height) {
+        } if (emailPosition.x + 80 > videoRect.width || emailPosition.x + 80 > videoRect.height) {
           console.log(true)
           setEmailPosition({
-            x:-10,
+            x: -10,
             y,
             directionX: emailPosition.directionX,
             directionY: -emailPosition.directionY,
           });
         } else {
-          setEmailPosition({ x:emailPosition.x+40, y:emailPosition.x+40, directionX: emailPosition.directionX, directionY: emailPosition.directionY });
+          setEmailPosition({ x: emailPosition.x + 40, y: emailPosition.x + 40, directionX: emailPosition.directionX, directionY: emailPosition.directionY });
         }
       };
       timeoutId = setInterval(updateEmailPosition, 1400);
@@ -62,56 +61,40 @@ console.log(emailPosition)
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{
-       position: "relative" ,
-       zIndex:1,
-       width:"900px",
-       height:"600px",
+        position: "relative",
+        zIndex: 1,
+        width: "900px",
+        height: "600px",
       }}>
-     
-      {/* <video
-      style={{
-       width:"900px",
-       height:"600px",
-      }}
-        ref={videoRef}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        onEnded={() => setIsPlaying(false)}
-        controls
-      >
-        
-        <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
-      <Player
-      ref={videoRef}
-      onPlay={() => setIsPlaying(true)}
-      onPause={() => setIsPlaying(false)}
-      onEnded={() => setIsPlaying(false)}
-          style={{
-            width: "900px",
-            height: "600px",
-          }}
-          
-          playsInline
-          poster="/assets/poster.png"
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        />
-      {isPlaying && (
-        <div
-          style={{
-            position: "absolute",
-            top: `${videoRef.current.offsetTop + emailPosition.y +10 }px`,
-            left: `${videoRef.current.offsetLeft + emailPosition.x +10}px`,
-            color:"red",
-            padding: "10px",
-            zIndex:2,
-          }}
-        >
-          Email: info@example.com
-        </div>
-      )}
-    </div>
+
+        <video ref={videoRef} style={{
+          position: "relative",
+          zIndex: 1,
+
+        }}
+          onPlay={() => setIsPlaying(true)}
+          onPause={() => setIsPlaying(false)}
+          onEnded={() => setIsPlaying(false)} controls className="abp-video abp-video--fluid" preload="metadata" width="1920" height="1080">
+          <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" type="video/mp4" />
+
+        </video>
+        {isPlaying && (
+          <p
+            style={{
+              position: "absolute",
+              top: `${videoRef.current.offsetTop + emailPosition.y + 10}px`,
+              left: `${videoRef.current.offsetLeft + emailPosition.x + 10}px`,
+              color: "red",
+              padding: "10px",
+              zIndex: 3,
+            }}
+          >
+            Email: info@example.com
+          </p>
+        )}
+      </div>
+      <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+
     </>
   )
 }
